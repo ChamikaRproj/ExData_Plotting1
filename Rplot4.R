dd5abee322d00908eb5b18441811c37999f4dc8a
ChamikaRproj <m29718@pgim.cmb.ac.lk>
2020-07-28 16:57:23 +0530

    Rplot 4 Commit

---
+++ Rplot4.R
@@ -0,0 +1,28 @@
+## Rplot 2
+## set working directorty via set(wd)
+datafull <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+subdata1 <- subset(datafull, Date %in% c("1/2/2007","2/2/2007"))
+subdata1$Date <- as.Date(subdata1$Date, format="%d/%m/%Y")
+datetime<-paste(as.Date(subdata1$Date), subdata1$Time)
+subdata1$Datetime<-as.POSIXct(datetime)
+
+#plotting
+
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(subdata1, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global_rective_power (kilowatts)",xlab="")
+})
+#file finishing
+dev.copy(png, file="Rplot4.png", height=480, width=480)
+dev.off()
